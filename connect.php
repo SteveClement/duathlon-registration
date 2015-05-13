d1799c5b9eed32ee5bc6bd1e309c301ba005f1d3
Steve Clement <steve@localhost.lu>
2015-05-13 16:28:03 +0200

    - rtfm… mysqli syntax changes

--- connect.php
+++ connect.php
@@ -1,9 +1,9 @@
 <?php
 $connection = mysqli_connect('localhost', 'MD2015', 'wellYouStoleThisUnprivil3ged ASSw0rd');
 if (!$connection){
-    die("Database Connection Failed" . mysqli_error());
+    die("Database Connection Failed" . mysqli_error($connection));
 }
-$select_db = mysqli_select_db('dbMD15');
+$select_db = mysqli_select_db($connection, 'dbMD15');
 if (!$select_db){
-    die("Database Selection Failed" . mysqli_error());
+    die("Database Selection Failed" . mysqli_error($connection));
 }
