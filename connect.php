7f06777aac7042ff4b712d99fa9500bc651b65de
Steve Clement <steve@localhost.lu>
2015-05-13 16:32:43 +0200

    - removed connect.php

--- connect.php
+++
@@ -1,9 +0,0 @@
-<?php
-$connection = mysqli_connect('localhost', 'MD2015', 'wellYouStoleThisUnprivil3ged ASSw0rd');
-if (!$connection){
-    die("Database Connection Failed" . mysqli_error($connection));
-}
-$select_db = mysqli_select_db($connection, 'dbMD15');
-if (!$select_db){
-    die("Database Selection Failed" . mysqli_error($connection));
-}
