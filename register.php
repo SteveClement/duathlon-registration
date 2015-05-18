70aaa8b128ba50a8408070987f9a87af0f1e9b88
Thierry Degeling <thierry@syn2cat.lu>
2015-05-18 21:00:16 +0200

    put all the categories into an array

--- register.php
+++ register.php
@@ -11,6 +11,28 @@ if (!$select_db){
     die("Database Selection Failed" . mysqli_error($connection));
 }
 
+    $categories = array(
+        "c8_9"      =>  "8&9 Kids B (2006-2007)",
+        "c10_11"    =>  "10&11 Kids A (2004-2005)",
+        "c12_13"    =>  "12&13 Youth C (2002-2003)",
+        "c14_15"    =>  "14&15 Youth B (2000-2001)",
+        "c16_17"    =>  "16&17 Youth A (1998-1999)",
+        "c18_19"    =>  "18&19 Junior (1997-1997)",
+        "c18_24"    =>  "18-24 mixed (1991-1997)",
+        "c25_29"    =>  "27-29 mixed (1990-1986)",
+        "c30_34"    =>  "30-34 mixed (1985-1981)",
+        "c35_39"    =>  "35-39 mixed (1980-1976)",
+        "c40_44"    =>  "40-44 mixed (1975-1971)",
+        "c45_49"    =>  "45-49 mixed (1970-1966)",
+        "c50_54"    =>  "50-54 mixed (1965-1961)",
+        "c55_59"    =>  "55-59 mixed (1960-1956)",
+        "c60_64"    =>  "60-64 mixed (1955-1951)",
+        "c65_69"    =>  "65-69 mixed (1950-1946)",
+        "c70_74"    =>  "70-74 mixed (1945-1941)",
+        "c75_79"    =>  "75-79 mixed (1940-1936)",
+        "c80_"      =>  "80+ mixed (1935 and earlier)"
+    );
+
     // If the values are posted, insert them into the database.
     if (
             isset( $_POST[ 'category'   ] )
@@ -36,65 +58,7 @@ if (!$select_db){
         $club       = $_POST[ 'club'        ];
         $comment    = $_POST[ 'comment'     ];
 
-        switch ($category) {
-            case "c8_9":
-                $ncat = "8&9 Kids B (2006-2007)";
-                break;
-            case "c10_11":
-                $ncat = "10&11 Kids A (2004-2005)";
-                break;
-            case "c12_13":
-                $ncat = "12&13 Youth C (2002-2003)";
-                break;
-            case "c14_15":
-                $ncat = "14&15 Youth B (2000-2001)";
-                break;
-            case "c16_17":
-                $ncat = "16&17 Youth A (1998-1999)";
-                break;
-            case "c18_19":
-                $ncat = "18&19 Junior (1997-1997)";
-                break;
-            case "c18_24":
-                $ncat = "18-24 mixed (1991-1997)";
-                break;
-            case "c25_29":
-                $ncat = "27-29 mixed (1990-1986)";
-                break;
-            case "c30_34":
-                $ncat = "30-34 mixed (1985-1981)";
-                break;
-            case "c35_39":
-                $ncat = "35-39 mixed (1980-1976)";
-                break;
-            case "c40_44":
-                $ncat = "40-44 mixed (1975-1971)";
-                break;
-            case "c45_49":
-                $ncat = "45-49 mixed (1970-1966)";
-                break;
-            case "c50_54":
-                $ncat = "50-54 mixed (1965-1961)";
-                break;
-            case "c55_59":
-                $ncat = "55-59 mixed (1960-1956)";
-                break;
-            case "c60_64":
-                $ncat = "60-64 mixed (1955-1951)";
-                break;
-            case "c65_69":
-                $ncat = "65-69 mixed (1950-1946)";
-                break;
-            case "c70_74":
-                $ncat = "70-74 mixed (1945-1941)";
-                break;
-            case "c75_79":
-                $ncat = "75-79 mixed (1940-1936)";
-                break;
-            case "c80_":
-                $ncat = "80+ mixed (1935 and earlier)";
-                break;
-            }
+        $ncat = $categories[ $category ];
 
         $query = sprintf("INSERT INTO tblregistrations (category, fname, street, postcode, locality, country, tel, email, dob, sex, license, club, comment) VALUES ('%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s')", mysqli_real_escape_string($connection, $category) , mysqli_real_escape_string($connection, $fname), mysqli_real_escape_string($connection, $street), mysqli_real_escape_string($connection, $postcode), mysqli_real_escape_string($connection, $locality), mysqli_real_escape_string($connection, $country), mysqli_real_escape_string($connection, $tel), mysqli_real_escape_string($connection, $email),  mysqli_real_escape_string($connection, $dob), mysqli_real_escape_string($connection, $sex), mysqli_real_escape_string($connection, $license), mysqli_real_escape_string($connection, $club), mysqli_real_escape_string($connection, $comment));
 
@@ -146,25 +110,13 @@ if (!$select_db){
 <h1>Register</h1>
 <form action="" method="POST">
     <p><label>Categories:</label> <br>
-    <input id="category" type="radio" name="category" value="c8_9"> 8&amp;9 Kids B (2006-2007) <br>
-    <input id="category" type="radio" name="category" value="c10_11"> 10&amp;11 Kids A (2004-2005) <br>
-    <input id="category" type="radio" name="category" value="c12_13"> 12&amp;13 Youth C (2002-2003) <br>
-    <input id="category" type="radio" name="category" value="c14_15"> 14&amp;15 Youth B (2000-2001) <br>
-    <input id="category" type="radio" name="category" value="c16_17"> 16&amp;17 Youth A (1998-1999) <br>
-    <input id="category" type="radio" name="category" value="c18_19"> 18&amp;19 Junior (1997-1997) <br>
-    <input id="category" type="radio" name="category" value="c18_24"> 18-24 mixed (1991-1997) <br>
-    <input id="category" type="radio" name="category" value="c25_29"> 27-29 mixed (1990-1986) <br>
-    <input id="category" type="radio" name="category" value="c30_34"> 30-34 mixed (1985-1981) <br>
-    <input id="category" type="radio" name="category" value="c35_39"> 35-39 mixed (1980-1976) <br>
-    <input id="category" type="radio" name="category" value="c40_44"> 40-44 mixed (1975-1971) <br>
-    <input id="category" type="radio" name="category" value="c45_49"> 45-49 mixed (1970-1966) <br>
-    <input id="category" type="radio" name="category" value="c50_54"> 50-54 mixed (1965-1961) <br>
-    <input id="category" type="radio" name="category" value="c55_59"> 55-59 mixed (1960-1956) <br>
-    <input id="category" type="radio" name="category" value="c60_64"> 60-64 mixed (1955-1951) <br>
-    <input id="category" type="radio" name="category" value="c65_69"> 65-69 mixed (1950-1946) <br>
-    <input id="category" type="radio" name="category" value="c70_74"> 70-74 mixed (1945-1941) <br>
-    <input id="category" type="radio" name="category" value="c75_79"> 75-79 mixed (1940-1936) <br>
-    <input id="category" type="radio" name="category" value="c80_"> 80+ mixed (1935 and earlier)
+    <?php
+        foreach ( $categories as $categoryId => $categoryName ) {
+
+            echo '<input id="category" type="radio" name="category" value="' . $categoryId . '"> ' . $categoryName . '<br />';
+
+        }
+    ?>
     </p>
     <p><label>Full Name:&nbsp;</label>
     <input id="fname" type="text" name="fname" placeholder="Numm Virnumm" /></p>
