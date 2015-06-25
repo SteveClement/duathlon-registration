374a092ab6e47cc2f9191ed9731465f4c664783b
Steve Clement <steve@localhost.lu>
2015-06-26 00:33:44 +0200

    - Initialized vars and put increments in

--- athletes.php
+++ athletes.php
@@ -60,6 +60,8 @@ require('connect.php');
 $sql = "SELECT id, category, fname, street, postcode, locality, country, tel, email, dob, sex, license, club, comment FROM tblregistrations where active=1 order by category";
 $result = $connection->query($sql);
 
+$c7a = $c8_9 = $c10_11 = $c12_13 = $c14_15 = $c16_17 = $c18_19 = $c18_24 = $c25_29 = $c30_34 = $c35_39 = $c40_44 = $c45_49 = $c50_54 = $c55_59 = $c60_64 = $c65_69 = $c70_74 = $c75_79 = $c80_ = 0;
+
 if ($result->num_rows > 0) {
   // output data of each row
   while($row = $result->fetch_assoc()) {
@@ -69,60 +71,79 @@ if ($result->num_rows > 0) {
         $c7A++;
       case "c8_9":
         $ncat ="8&9 Kids B (2006-2007)";
+        $c8_9++;
         break;
       case "c10_11":
         $ncat ="10&11 Kids A (2004-2005)";
+        $c10_11++;
         break;
       case "c12_13":
         $ncat ="12&13 Youth C (2002-2003)";
+        $c12_13++;
         break;
       case "c14_15":
         $ncat ="14&15 Youth B (2000-2001)";
+        $c14_15++;
         break;
       case "c16_17":
         $ncat ="16&17 Youth A (1998-1999)";
+        $c16_17++;
         break;
       case "c18_19":
         $ncat ="18&19 Junior (1997-1997)";
+        $c18_19++;
         break;
       case "c18_24":
         $ncat ="18-24 mixed (1991-1997)";
+        $c18_24++;
         break;
       case "c25_29":
         $ncat ="27-29 mixed (1990-1986)";
+        $c25_29++;
         break;
       case "c30_34":
         $ncat ="30-34 mixed (1985-1981)";
+        $c30_34++;
         break;
       case "c35_39":
         $ncat ="35-39 mixed (1980-1976)";
+        $c35_39++;
         break;
       case "c40_44":
         $ncat ="40-44 mixed (1975-1971)";
+        $c40_44++;
         break;
       case "c45_49":
         $ncat ="45-49 mixed (1970-1966)";
+        $c45_49++;
         break;
       case "c50_54":
         $ncat ="50-54 mixed (1965-1961)";
+        $c50_54++;
         break;
       case "c55_59":
         $ncat ="55-59 mixed (1960-1956)";
+        $c55_59++;
         break;
       case "c60_64":
         $ncat ="60-64 mixed (1955-1951)";
+        $c60_64++;
         break;
       case "c65_69":
         $ncat ="65-69 mixed (1950-1946)";
+        $c65_69++;
         break;
       case "c70_74":
         $ncat ="70-74 mixed (1945-1941)";
+        $c70_74++;
         break;
       case "c75_79":
         $ncat ="75-79 mixed (1940-1936)";
+        $c75_79++;
         break;
       case "c80_":
         $ncat ="80+ mixed (1935 and earlier)";
+        $c80_++;
         break;
       }
     echo "<tr>";
